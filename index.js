73f8dca2fcebfe130decbaab5f50c4af5bf646f2
AlanZou007 <18390410727@163.com>
2017-12-08 11:22:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {cookieConfig} from "XX";
+
 const localName = 'clinic__' // 存入localStorage属性名
 
 export const is_IOS = typeof window !== 'undefined' && /(iPhone|iPad|iPod|iOS)/i.test(window.navigator.userAgent)
@@ -45,3 +47,42 @@ export function loadFromLocal(id, key, def) {
     let ret = store[key];
     return ret || def;
 };
+
+export function readCookie(name) {
+    let nameEQ = `${cookieConfig.prefix}_${name}=`;
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        // eslint-disable-next-line
+        if (c.charAt(0) == ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
+}
+
+export function removeCookie(name) {
+    let nameEQ = `${cookieConfig.prefix}_${name}`;
+    saveCookie(nameEQ, "", -1);
+}
+
+export function urlParse() {
+    const url = window.location.search;
+    const obj = {};
+    const reg = /[?&][^?&]+=[^?&]+/g;
+    const arr = url.match(reg);
+    // ['?id=12345', '&a=b']
+    if (arr) {
+        arr.forEach((item) => {
+            const tempArr = item.substring(1).split('=');
+            /* 编码decodeURIComponent */
+            const key = decodeURIComponent(tempArr[0]);
+            const val = decodeURIComponent(tempArr[1]);
+            obj[key] = val;
+        });
+    }
+    return obj;
+}
