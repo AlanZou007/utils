7230ce7776318651fcc72eb54de5e1a463dac553
azou <zouxin@kkguan.com>
2018-04-23 16:01:32 +0800

    add wxacode

--- dom.js
+++ dom.js
@@ -1,10 +1,20 @@
-// testkmjkzx.kmwlyy.com/web/shop/wx/userAuthentication?orgId=DIDI&openId=110100192001016715&sex=1&tel=13751096562&groupId=test2&email=181029336%40qq.com&birthday=1984-09-15&trueName=maituwang&nickName=赵俊&city
-
+/**
+ * 判断dom是否有该类名
+ * 
+ * @param {dom} el 
+ * @param {string} className 
+ */
 export function hasClass(el, className) {
     let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
     return reg.test(el.className)
 }
 
+/**
+ * 添加类名
+ * 
+ * @param {dom} el 
+ * @param {string} className 
+ */
 export function addClass(el, className) {
     if (hasClass(el, className)) {
         return
@@ -14,6 +24,12 @@ export function addClass(el, className) {
     el.className = newClass.join(' ')
 }
 
+/**
+ * 删除类名
+ * 
+ * @param {dom} el 
+ * @param {string} className 
+ */
 export function removeClass(el, className) {
     if (!hasClass(el, className)) {
         return
@@ -23,6 +39,13 @@ export function removeClass(el, className) {
     el.className = el.className.replace(reg, ' ')
 }
 
+/**
+ * 设置过去自定义属性
+ * 
+ * @param {dom} el 
+ * @param {string} name 
+ * @param {*} val 
+ */
 export function getData(el, name, val) {
     let prefix = 'data-'
     if (val) {
@@ -31,6 +54,11 @@ export function getData(el, name, val) {
     return el.getAttribute(prefix + name)
 }
 
+/**
+ * 获取元素大小 位置信息
+ * 
+ * @param {dom} el 
+ */
 export function getRect(el) {
     if (el instanceof window.SVGElement) {
         let rect = el.getBoundingClientRect()
