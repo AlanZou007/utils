88ff46543d6b3adbbb63b6b9c6ccc369447ec0b7
azou <Alanuuu@163.com>
2018-09-17 17:37:28 +0800

    feat(netWork): Export instantiation

--- network.js
+++ network.js
@@ -4,24 +4,24 @@ class Network {
         this.callback = callback;
         this._init();
     }
-    
+
     _init() {
         window.addEventListener('online', () =>{
             this._fnNetworkHandler();
         }, true);
-        
+
         window.addEventListener('offline', () => {
             this._fnNetworkHandler();
         }, true);
     }
-    
+
     _fnNetworkHandler() {
         this.callback && this.callback(this.navigator.onLine ? 'online' : 'offline') ;
     }
-    
+
     isOnline() {
         return this.navigator.onLine ;
     }
 }
 
-export default Network
+export default new Network()
